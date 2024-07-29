f5aca2961fd6afd2511597d4258d97b0f8429e94
ElliottCastro06 <105030067+ElliottCastro06@users.noreply.github.com>
2024-07-28 18:00:33 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function redirectThanks(thankYouPage) {
-    window.location.href = thankYouPage;
-    return false;  // Prevent form submission
+function comprar(plan, precio) {
+    alert('Has comprado el plan ' + plan + ' por ' + precio + ' colones.');
+    // Aquí puedes agregar la lógica para redirigir a la página de agradecimiento correspondiente
+    window.location.href = 'thanks-' + plan.replace(' ', '-').toLowerCase() + '.html';
 }
